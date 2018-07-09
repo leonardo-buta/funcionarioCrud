1cc8ac5b4527880f629bf6730a3ff831846497e6
leonardolima85 <leonardo@scrdigital.com>
2018-07-09 12:38:16 -0300

    Adicionado a estrutura do projeto inicial DDD

---
+++ WebAPI/FuncionarioApi/Funcionario.Infra.Data/Context/FuncionarioContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Funcionario.Infra.Data.Context
+{
+    public class FuncionarioContext : DbContext
+    {
+        public FuncionarioContext()
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+    }
+}
